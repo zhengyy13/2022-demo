d5ffaf98f765ba6a091188d29ef19fc5cf30c759
zhengyy1 <zhengyy1@wedoctor.com>
2023-03-09 09:44:09 +0800

    test

---
+++ start/src/test/java/com/d2y/demo/Java8Test.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2001-2023 GuaHao.com Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of GuaHao Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package com.d2y.demo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO
+ *
+ * @author zhengyy1
+ * @version V1.0
+ * @since 2023-03-09 09:40
+ */
+public class Java8Test {
+
+    @Test
+    public void test01() {
+
+    }
+}
